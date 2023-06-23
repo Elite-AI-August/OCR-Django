e353ee34198d820521f0c77a1951ec9b594263d2
CarollCheng <carollhwrd@gmail.com>
2023-06-23 20:35:24 +0530

    Add files via upload

---
+++ ocr/views.py
@@ -0,0 +1,32 @@
+import base64
+
+import numpy as np
+import pytesseract
+from django.contrib import messages
+from django.shortcuts import render
+from PIL import Image
+
+# you have to install tesseract module too from here - https://github.com/UB-Mannheim/tesseract/wiki
+pytesseract.pytesseract.tesseract_cmd = (
+    r"C:\Program Files\Tesseract-OCR\tesseract.exe"  # Path to tesseract.exe
+)
+
+
+def homepage(request):
+    if request.method == "POST":
+        try:
+            image = request.FILES["imagefile"]
+            # encode image to base64 string
+            image_base64 = base64.b64encode(image.read()).decode("utf-8")
+        except:
+            messages.add_message(
+                request, messages.ERROR, "No image selected or uploaded"
+            )
+            return render(request, "home.html")
+        lang = request.POST["language"]
+        img = np.array(Image.open(image))
+        text = pytesseract.image_to_string(img, lang=lang)
+        # return text to html
+        return render(request, "home.html", {"ocr": text, "image": image_base64})
+
+    return render(request, "home.html")
---
+++ static/copy.js
@@ -0,0 +1,20 @@
+console.log('copy.js');
+
+// copy to clipboard on copy btn click
+function copy() {
+    /* Get the text field */
+    var copyText = document.getElementById("ocrtext");
+    // get text inside of copytext element
+    var text = copyText.innerText;
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(text);
+    // set the text to be copied
+    document.querySelector('#copybtn').innerText = "Copied!";
+}
+
+
+// on upload button click , change text to clicked
+function uploaded() {
+    console.log('uploaded');
+    document.querySelector('#upload-btn').innerText = "Uploaded!";
+}
